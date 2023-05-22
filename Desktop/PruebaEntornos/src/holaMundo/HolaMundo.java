05a7047a256c77c39a202d56513555661589f18d
DavidLopezGalileo <davidlob@alumnos.iesgalileo.es>
2023-05-22 18:09:49 +0200

    Segunda version de Hola Mundo

--- Desktop/PruebaEntornos/src/holaMundo/HolaMundo.java
+++ Desktop/PruebaEntornos/src/holaMundo/HolaMundo.java
@@ -11,5 +11,6 @@ package holaMundo;
 public class HolaMundo {
     public static void main(String[] args){
         System.out.println("Hola Mundo");
+        System.out.println("Adios Planeta");
     }
 }
